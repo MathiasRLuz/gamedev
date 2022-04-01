0305a60d5ded64cf20a2d4df534046b5058fcc19
Mathias Rodrigues da Luz <68118121+MathiasRLuz@users.noreply.github.com>
2022-04-01 20:07:08 -0300

    Update ScriptImporter.lua

--- Roblox Scripts/ScriptImporter.lua
+++ Roblox Scripts/ScriptImporter.lua
@@ -1,17 +1,29 @@
-s.Disabled = false
-player.CharacterAdded:connect(function (char)
-    local s = script.SprintScript:clone()
-    s.Parent = player.Character
-    s.Disabled = false
-    --[[
-    local s = script.TimeLoop:clone()
-    s.Parent = char
-    s.Disabled = false			
-    ]]
-    local s = script.Abilities:clone()
-    s.Parent = player.Character
-    s.Disabled = false
-end)
+function onPlayerEntered(player)
+	repeat wait () until player.Character ~= nil
+	local s = script.SprintScript:clone()
+	s.Parent = player.Character
+	s.Disabled = false
+	--[[
+	local s = script.TimeLoop:clone()
+	s.Parent = player.Character
+	s.Disabled = false
+	]]
+	local s = script.Abilities:clone()
+	s.Parent = player.Character
+	s.Disabled = false
+	player.CharacterAdded:connect(function (char)
+		local s = script.SprintScript:clone()
+		s.Parent = player.Character
+		s.Disabled = false
+		--[[
+		local s = script.TimeLoop:clone()
+		s.Parent = char
+		s.Disabled = false			
+		]]
+		local s = script.Abilities:clone()
+		s.Parent = player.Character
+		s.Disabled = false
+	end)
 end
 
 game.Players.PlayerAdded:connect(onPlayerEntered)
