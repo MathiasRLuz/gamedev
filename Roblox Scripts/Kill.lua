222278ca11d4b028dc17fded2cdb2821723992fb
Mathias Rodrigues da Luz <68118121+MathiasRLuz@users.noreply.github.com>
2022-04-12 14:27:45 -0300

    update

--- Roblox Scripts/Kill.lua
+++ Roblox Scripts/Kill.lua
@@ -1,5 +1,5 @@
 script.Parent.Touched:connect(function(hit)
 	if hit and hit.Parent and hit.Parent:FindFirstChild("Humanoid") then
-		hit.Parent.Humanoid.Health = 0
+		hit.Parent.Humanoid.Health -= 100
 	end
 end)
