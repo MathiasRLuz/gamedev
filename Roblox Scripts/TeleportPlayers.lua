765d8b5e0c1d81d7de5068c56caa55e267dd2f76
Mathias Rodrigues da Luz <68118121+MathiasRLuz@users.noreply.github.com>
2022-05-27 19:09:13 -0300

    Update TeleportPlayers.lua

--- Roblox Scripts/TeleportPlayers.lua
+++ Roblox Scripts/TeleportPlayers.lua
@@ -1,16 +1,19 @@
 local RunService = game:GetService("RunService")
 local tempo = 0
-local minPlayers = 2
+local minPlayers = 1
 local tempoIniciar = 10
 local iniciar = false
 local iniciou = false
 
-local jogos = {game.Workspace.Spawns.SpawnCorrida,game.Workspace.Spawns.SpawnLabirinto} -- lista de spawns de minigames
+local jogos = {game.Workspace.Spawns.SpawnCorrida,game.Workspace.Spawns.SpawnLabirinto,game.Workspace.Spawns.SpawnFutebol} -- lista de spawns de minigames
 
 function teleportar(jogo)
 	for i,player in ipairs(game.Players:GetPlayers()) do			
-		if player:GetAttribute("Pronto") == true then			
-			local character = player.Character			
+		if player:GetAttribute("Pronto") == true then	
+			if game.Workspace.Button:GetAttribute("Jogo") == "Labirinto" then
+				player.CameraMode = Enum.CameraMode.LockFirstPerson
+			end
+				local character = player.Character			
 			character:PivotTo(CFrame.new(jogo.Position)+Vector3.new(0,4,0))
 		end							
 	end
@@ -25,25 +28,28 @@ RunService.Stepped:Connect(function(time_,deltaTime)
 			iniciou = true
 			game.Workspace.Eventos.StartMiniGame:Fire()
 			local indice = math.random(1,#jogos)
-			--indice = 1 -- comentar essa linha depois
+			indice = 3 -- comentar essa linha depois
 			if indice == 1 then
 				game.Workspace.Button:SetAttribute("Jogo","Corrida")				
 			elseif indice == 2 then
-				game.Workspace.Button:SetAttribute("Jogo","Labirinto")			
+				game.Workspace.Button:SetAttribute("Jogo","Labirinto")		
+			elseif indice == 3 then
+				game.Workspace.Button:SetAttribute("Jogo","Futebol")	
 			end
 			teleportar(jogos[indice]) -- teleporta para um minigame aleatório
 		end
 	end
 	if tempo > 10 and not iniciar then
 		tempo = 0		
-		local prontos = true
+		local prontos = 0
 		for i,player in ipairs(game.Players:GetPlayers()) do			
 			if player:GetAttribute("Pronto") == false then
 				print(player.Name .. " não está pronto")
-				prontos = false
+			else
+				prontos += 1
 			end							
 		end
-		if prontos and game.Players.NumPlayers >= minPlayers then
+		if prontos >= minPlayers then
 			print("O jogo vai começar em " .. tempoIniciar .." segundos")
 			iniciar = true
 			game.Workspace.Eventos.IniciarTimer:FireAllClients()
@@ -56,9 +62,10 @@ game.Workspace.Eventos.FimMiniGame.Event:Connect(function()
 	iniciou = false
 	tempo = 0
 	for i,player in ipairs(game.Players:GetPlayers()) do			
-		if player:GetAttribute("Pronto") == true then			
+		if player:GetAttribute("Pronto") == true then				
 			local character = player.Character			
 			player:SetAttribute("Pronto", false)
+			player.CameraMode = Enum.CameraMode.Classic
 			character:PivotTo(CFrame.new(game.Workspace.Spawns.SpawnLocation.Position))
 		end							
 	end
